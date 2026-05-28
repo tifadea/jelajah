784e3f618c3b55e5ce40bf1bac8722550e479db2
tifadea <abahmamat061@gmail.com>
2026-05-28 19:08:49 +0700

    Fix case sensitivity in login redirect URL

--- js/welcome.js
+++ js/welcome.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function goLogin() {
-        window.location.href = "login/index.html";
+        window.location.href = "Login/index.html";
     }
 
     function logout() {
